6baf9ccdd8aee3158c11d23df2d9e539bfed580f
Андрей Бизюк <biziuk.andrei@gmail.com>
2019-10-14 13:28:44 +0300

    Удалил лишний контроллер

--- src/controller/ItemController.js
+++
@@ -1,9 +0,0 @@
-import { EventEmitter } from 'events';
-
-export default class ItemController extends EventEmitter {
-  constructor(model, view) {
-    super();
-    this.model = model;
-    this.view = view;
-  }
-}
